d435da17c290b6271a4a5cacf893dd0bb087c539
Daniel Ahavat <daniel.ahavat@gmail.com>
2021-03-22 16:57:39 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/daniel/services/CategoriaService.java
+++ src/main/java/com/daniel/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.daniel.domain.Categoria;
+import com.daniel.dto.CategoriaDTO;
 import com.daniel.repositories.CategoriaRepository;
 import com.daniel.services.exceptions.DataIntegrityException;
 import com.daniel.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
